3ba2fee45e3d6e0ef9106caf3ebd1eb961b11db9
tqk2811 <tqk2811@gmail.com>
2026-03-02 11:25:16 +0700

    fix: mark DesktopDuplicationCapture as NOT_IMPLEMENTED, use FALSE instead of NULL for BOOL

--- TqkLibrary.WindowCapture.Native/DesktopDuplicationCapture.cpp
+++ TqkLibrary.WindowCapture.Native/DesktopDuplicationCapture.cpp
@@ -16,13 +16,14 @@ DesktopDuplicationCapture::~DesktopDuplicationCapture()
 
 }
 
+// NOT_IMPLEMENTED: Desktop Duplication API capture is not yet implemented
 BOOL DesktopDuplicationCapture::Draw(ID3D11Device* device, ID3D11DeviceContext* deviceCtx, ComPtr<ID3D11Texture2D>& textureBGRA)
 {
-	return NULL;
+	return FALSE; // NOT_IMPLEMENTED
 }
 HBITMAP DesktopDuplicationCapture::Shoot()
 {
-	return NULL;
+	return NULL; // NOT_IMPLEMENTED
 }
 
 
@@ -39,17 +40,17 @@ BOOL DesktopDuplicationCapture::InitMonitorCapture(HMONITOR hmonitor)
 }
 BOOL DesktopDuplicationCapture::GetSize(UINT32& width, UINT32& height)
 {
-	return NULL;
+	return FALSE; // NOT_IMPLEMENTED
 }
 BOOL DesktopDuplicationCapture::Render(IDXGISurface* surface, bool isNewSurface, bool& isNewtargetView)
 {
-	return NULL;
+	return FALSE; // NOT_IMPLEMENTED
 }
 BOOL DesktopDuplicationCapture::CaptureImage(void* data, UINT32 width, UINT32 height, UINT32 linesize)
 {
-	return NULL;
+	return FALSE; // NOT_IMPLEMENTED
 }
 BOOL DesktopDuplicationCapture::IsSupported()
 {
-	return NULL;
+	return FALSE; // NOT_IMPLEMENTED
 }
